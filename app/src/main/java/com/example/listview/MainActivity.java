8b38257c233f7b6196400ec45b2baa8f2ecca82c
Asi_stas <potekhina15092003@gmail.com>
2022-12-07 11:05:03 +0300

    ListView. Часть 2

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,15 +22,29 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
 
         ArrayList<String> posts = new ArrayList<>();
-        for (int i = 0; i < 20; i++){
+        /*for (int i = 0; i < 20; i++){
             posts.add("Post " + i);
             Log.i("posts: ", posts.get(i));
 
-        }
-
+        }*/
+        posts.add("Красный");
+        posts.add("Оранжевый");
+        posts.add("Желтый");
+        posts.add("Зеленый");
+        posts.add("Голубой");
+        posts.add("Синий");
+        posts.add("Фиолетовый");
 
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, posts);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //view.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Номер " + position + " - " + posts.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
